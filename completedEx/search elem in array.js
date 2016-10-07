a030e41591ef67f3e02306d0a586a3165f5cc9c3
Mihail <skyhord@gmail.com>
2016-10-07 20:52:47 +0500

    Update search elem in array.js

--- completedEx/search elem in array.js
+++ completedEx/search elem in array.js
@@ -1,14 +1,10 @@
 var fruits = ["apricot", "peach","apple", "orange", "kiwi"];
 function find(arr,value){
-	for (i=0; 0<arr.length; i++){
+	for (i=0; i<arr.length; i++){
 		if(arr[i] === value){
 			return [i];
-		} else if (i===arr.length){
-			return -1;
-		}
+		} 
 	}
-	
+	return -1;
 }
-find (fruits, "orange");
-
-//potencial infinity loop??
+find (fruits, "oradnge");
