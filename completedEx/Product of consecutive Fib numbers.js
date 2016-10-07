bfaf5f50d3dacecb8fc6e4aa8a6cd73b2d846cf6
Mihail <skyhord@gmail.com>
2016-10-07 13:26:51 +0500

    Update Product of consecutive Fib numbers.js

--- completedEx/Product of consecutive Fib numbers.js
+++ completedEx/Product of consecutive Fib numbers.js
@@ -4,10 +4,10 @@ var a,b;
 	for (var i = 0; i < prod; i++){
 	a = fib[i], b = fib[i+1];
 	fib.push(a+b);
-	if(a * b === prod) {
-	return [a, b, true];
-	} else if (a*b >prod){
-	return [a, b, false];
-	}	
-}
+		if(a * b === prod) {
+		return [a, b, true];
+		} else if (a*b >prod){
+		return [a, b, false];
+		}	
+	}
 }
