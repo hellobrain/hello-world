b52de732fb79bdb0baf42cea777dbb360419dee3
Mihail <skyhord@gmail.com>
2016-10-08 12:12:42 +0500

    Update calc array prompt test.js

--- completedEx/calc array prompt test.js
+++ completedEx/calc array prompt test.js
@@ -1,11 +1,13 @@
 var newArr=[];
 var num = 0;
-for (var i = 0; num >= 0; i++){
+var sum = 0;
+for (var i = 0; num >= 0 && num !== null && num !== "" && num !== undefined; i++){
 		num = prompt("Введите число");
-		if (isFinite(num) && num !== null && num !== ""){
+		if (isFinite(num) && num !== null && num !== "" && num !== undefined){
 			newArr.push(num);
+			sum += parseInt(newArr[i]);
+			num = 0;
 		} 
-			
 	}
 	
-console.log(newArr);
+console.log(sum);
