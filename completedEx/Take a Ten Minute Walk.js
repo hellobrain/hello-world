b3e29283853731e29b8f478dd8712fdb3d1db0ae
Mihail <skyhord@gmail.com>
2016-10-08 22:15:29 +0500

    Update Take a Ten Minute Walk.js

--- completedEx/Take a Ten Minute Walk.js
+++ completedEx/Take a Ten Minute Walk.js
@@ -1,25 +1,12 @@
 function isValidWalk(walk) {
   var time = 0;
-  
   walk.forEach(function (item,index){
   switch (item) {
-    case "n":
-    time++;
-    break;
-    
-    case "s":
-    time--;
-    break;
-    
-    case "w":
-    time += 2;
-    break;
-    
-    case "e":
-    time -= 2;
-    break;
-  }
-  });
+    case "n": time++; break;
+    case "s": time--; break;
+    case "w": time++; break;
+    case "e": time--; break;
+  }});
   
   if (time === 10 && walk.length === 10){
     return true;
