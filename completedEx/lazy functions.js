e62b6cc2877326957306e969c75bced02a8a0195
Mihail <skyhord@gmail.com>
2016-10-09 13:17:07 +0500

    Update lazy functions.js

--- completedEx/lazy functions.js
+++ completedEx/lazy functions.js
@@ -1,3 +1,13 @@
+var double = function (n) {
+  return n * 2;
+};
+var add = function (a, b) {
+  return a + b;
+};
+
+var lazy_value = make_lazy(double, 5);
+var lazy_sum = make_lazy(add, 2, 3);
+
 var make_lazy = function (func) {
   return func.bind.apply(func, arguments);
 };
