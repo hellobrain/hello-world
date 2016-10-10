c5e4b0b5d0949cad444785ca700956cd97df537a
Mihail <skyhord@gmail.com>
2016-10-10 18:12:16 +0500

    Update real shuffle array.js

--- completedEx/real shuffle array.js
+++ completedEx/real shuffle array.js
@@ -2,13 +2,13 @@ function shuffle (arr){
 	var randomIndex = 0;
 	var currentItem = 0;
 	
-	arr.forEach(function(e,i){
-		randomIndex = Math.floor(Math.random() * (arr.length -i) +i);
+	for (var i = arr.length -1; i > 0; i--){
+		randomIndex = Math.floor(Math.random() * (i +1));
 		currentItem = arr[i];
 		arr[i] = arr[randomIndex]; 
 		arr[randomIndex] = currentItem;
-	});
+	}
 	return arr;
 }
 
-shuffle([9, 3, 6, 0, 6, 4, 2, 1, 5, 7]);
+shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
