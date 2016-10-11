d0dd54de2cafc13813c051be565b3e70679545ee
Mihail <skyhord@gmail.com>
2016-10-11 14:18:03 +0500

    Create Search and Replace.js

---
+++ completedEx/freecodecamp/Search and Replace.js
@@ -0,0 +1,10 @@
+function myReplace(str, before, after) {
+  if(before[0] === before[0].toUpperCase()){
+    after = after[0].toUpperCase() + after.slice(1); 
+  } else {
+    after = after[0].toLowerCase() + after.slice(1);
+  }
+    return str.replace(before,after);
+}
+
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting");
