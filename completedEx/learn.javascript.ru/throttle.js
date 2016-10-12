0ecf3cc79a360a13d18cf21a212289e10fce747a
Mihail <skyhord@gmail.com>
2016-10-12 15:59:52 +0500

    Update throttle.js

--- completedEx/learn.javascript.ru/throttle.js
+++ completedEx/learn.javascript.ru/throttle.js
@@ -1,37 +1,74 @@
-//not ready
+// ??
 
+var log = "";
 var f = function(a) {
-  console.log(a);
+  log += a;
+  console.log(log);
 };
 
-function throttle (func, time){
-	var state = true,
-		savedArgs,
-		savedThis;
+// function throttle (func, time){
+// 	var state = true,
+// 		savedArgs,
+// 		savedThis;
 	
-	return function (a){
-		savedArgs = arguments;
-		savedThis = this;
+// 	return function (a){
 		
-		if (state){
-			state = false;
-			func.call(this, a);
+// 		if (state){
+// 			state = false;
+// 			func.call(this, a);
+// 			var timer = setTimeout(function(){
+// 				if(savedArgs){
+// 					func.apply(savedThis, savedArgs);
+// 					savedArgs = savedThis = null;
+// 				}
+// 				state = true;
+// 			}, time);
 			
-			var timer = setTimeout(function(){
-				func.apply(savedThis, savedArgs);
-				state = true;
-			}, time);
-		}
-	};
+// 		} else {
+// 			savedArgs = arguments;
+// 			savedThis = this;
+// 			return;
+// 		}
+// 	};
+// }
+
+function throttle(func, ms) {
+
+  var isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+
+    if (isThrottled) { // (2)
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments); // (1)
+
+    isThrottled = true;
+
+    setTimeout(function() {
+      isThrottled = false; // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
 }
 
 
 // затормозить функцию до одного раза в 1000 мс
 var f1000 = throttle(f, 1000);
 
-f1000(1); // выведет 1
-f1000(2); // (тормозим, не прошло 1000 мс)
-f1000(3); // (тормозим, не прошло 1000 мс)
-
-// когда пройдёт 1000 мс...
-// выведет 3, промежуточное значение 2 игнорируется
+f1000(1);
+f1000(2);
+f1000(3);
+setTimeout(f1000(4), 4000);
+setTimeout(f1000(5), 4000);
+setTimeout(f1000(6), 4000);
