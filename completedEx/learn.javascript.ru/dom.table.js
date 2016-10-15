cdc39a3a5f607b272b3f80616956b314d72d9b37
Mihail <skyhord@gmail.com>
2016-10-15 17:58:43 +0500

    Create dom.table.js

---
+++ completedEx/learn.javascript.ru/dom.table.js
@@ -0,0 +1,7 @@
+    var table = document.body.children[0];
+    var length = table.rows.length;
+    var i;
+    
+    for (i = 0; lengthR > 0; i++){
+      table.rows[i].cells[i].style.backgroundColor = "red";
+    }
