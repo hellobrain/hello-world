0aaf02b438e2f667c441894be4ee736ccd5c246d
Mihail <skyhord@gmail.com>
2016-10-18 17:40:12 +0500

    Create dom.create list from prompt.js

---
+++ completedEx/learn.javascript.ru/dom.create list from prompt.js
@@ -0,0 +1,28 @@
+      // http://learn.javascript.ru/task/create-list
+      var string;
+      var arr = [];
+
+      do { // запрашиваем названия элементов списка
+        string = prompt('Type something', 'text');
+        if(string) arr.push(string);
+      } while (string);
+
+      if (arr.length === 0) { // если ничего не было введено - пишем "пусто"
+        document.write('Empty');
+
+      } else {
+        var ul = document.createElement('ul');
+        var li = document.createElement('li');
+        var menu = document.querySelector('.menu').appendChild(ul);
+        var fragment = document.createDocumentFragment();
+
+        var i;
+
+        for (i = 0; i < arr.length; i++){
+          var clone = li.cloneNode(false);
+          clone.innerHTML = arr[i];
+          fragment.appendChild(clone);
+        }
+
+        menu.appendChild(fragment);
+    }
