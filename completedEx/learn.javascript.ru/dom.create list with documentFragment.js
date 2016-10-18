66d46425a46da0869f5b5186ea57feda5f831d1d
Mihail <skyhord@gmail.com>
2016-10-18 17:47:12 +0500

    Update and rename dom.create list. documentFragment.js to dom.create list with documentFragment.js

--- completedEx/learn.javascript.ru/dom.create list with documentFragment.js
+++ completedEx/learn.javascript.ru/dom.create list with documentFragment.js
@@ -18,21 +18,22 @@
       if (arr.length === 0) { // если ничего не было введено - пишем "пусто"
         document.write('Empty');
 
-      } else {
+      } else { // создаем необходимые елементы
         var ul = document.createElement('ul');
-        var li = document.createElement('li');
         var menu = document.querySelector('.menu').appendChild(ul);
+        
+        var li = document.createElement('li');
         var fragment = document.createDocumentFragment();
 
         var i;
 
-        for (i = 0; i < arr.length; i++){
+        for (i = 0; i < arr.length; i++){ // клонируем лишки и добавляем их в documentFragment
           var clone = li.cloneNode(false);
           clone.innerHTML = arr[i];
           fragment.appendChild(clone);
         }
 
-        menu.appendChild(fragment);
+        menu.appendChild(fragment); // добавляем полученный список к меню
     }
     </script>
     </div>
