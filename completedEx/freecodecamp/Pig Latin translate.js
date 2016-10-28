e1a92be454b68ae41ba02a943256dde3ecca912d
Mihail <skyhord@gmail.com>
2016-10-28 16:03:49 +0500

    Create Pig Latin translate.js

---
+++ completedEx/freecodecamp/Pig Latin translate.js
@@ -0,0 +1,28 @@
+// https://www.freecodecamp.com/challenges/pig-latin
+
+function translatePigLatin(str) {
+    var vowels = ['a', 'e', 'i', 'o', 'u'],
+        consonantCount = 0,
+        i;
+
+    for (i = 0; i < str.length; i++) {
+      
+        // vowels check
+        if (vowels.indexOf(str[i]) !== -1) {
+            
+            if (!consonantCount) {
+                return str + 'way';
+            } else { 
+              
+                // enter here if we meet vowels after consonant
+                return str.substr(consonantCount, str.length) + str.substr(0, consonantCount) + 'ay';
+            }
+          
+        
+        } else if (vowels.indexOf(str[i]) == -1) {
+            consonantCount++;
+        }
+    }
+}
+
+translatePigLatin("california");
