d8590bb4e3fbd7600aa312a6f87a4ac510816665
Mihail <skyhord@gmail.com>
2016-11-25 15:22:20 +0500

    Create Sum All Odd Fibonacci Numbers.js

---
+++ completedEx/freecodecamp/Sum All Odd Fibonacci Numbers.js
@@ -0,0 +1,35 @@
+// https://www.freecodecamp.com/challenges/sum-all-odd-fibonacci-numbers
+
+function generateFibArray(num) {
+	let arr = [],
+			first = 1,
+			second = 1;
+	
+	for (let i = 0; i <= num; i++) {
+		let sum = first + second;
+		
+		if(sum > num) {
+			return arr;
+		}
+		
+		arr.push(sum);
+		first = second;
+		second = arr[i];
+	}
+}
+
+function sumFibs(num) {
+	if(num === 1) return 1;
+	
+	return generateFibArray(num).reduce((p, n, i, arr) => {
+      
+		if(n % 2 === 0) {
+			return p;
+		}	else {
+			return p + n;
+		}
+	});
+}
+
+sumFibs(1);
+// should return 1785
