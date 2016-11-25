3d8387290cb3c97b02f6924f440ec7a552ee86d4
Mihail <skyhord@gmail.com>
2016-11-25 13:36:30 +0500

    Create Boo who.js

---
+++ completedEx/freecodecamp/Boo who.js
@@ -0,0 +1,8 @@
+// https://www.freecodecamp.com/challenges/boo-who
+
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return (bool === true || bool === false);
+}
+
+booWho(null);
